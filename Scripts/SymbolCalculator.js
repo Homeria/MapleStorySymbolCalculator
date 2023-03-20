fc649f28078a3050fed8d0ae49cc9efbc272e5d8
Homeria <ckrudgh77@naver.com>
2023-03-20 15:24:12 +0900

    Update 2023.03.20 - 2
    1. fixed bugs related to calculating symbol inholds
    2. modify design

--- Scripts/SymbolCalculator.js
+++ Scripts/SymbolCalculator.js
@@ -1,5 +1,4 @@
-const reqGrowthValues = [12, 15, 20, 27, 36, 47, 60, 75, 92, 111, 132, 155, 180, 207, 236, 267, 300, 335, 372];
-const cumulativeGrowthValue = [0, 12, 27, 47, 74, 110, 157, 217, 292, 384, 495, 627, 782, 962, 1169, 1405, 1672, 1972, 2307, 2679];
+const reqGrowthValues = [12, 15, 20, 27, 36, 47, 60, 75, 92, 111, 132, 155, 180, 207, 236, 267, 300, 335, 372, 0];
 const levelUpThreshold = [12, 27, 47, 74, 110, 157, 217, 292, 384, 495, 627, 782, 962, 1169, 1405, 1672, 1972, 2307, 2679];
 
 class Symbol {
@@ -9,12 +8,14 @@ class Symbol {
         this.in_holds = in_holds;
     }
 
-    getTotalInHolds() {
-        return (cumulativeGrowthValue[this.level - 1] + this.in_holds);
-    }
-
     getTotalReqSymbol() {
-        return (2679 - this.getTotalInHolds());
+        var total = 0;
+        for(var i = this.level; i <= 20; i++) {
+            console.log("total = " + total + ", i = " + i + ", reqGrowthValues[i - 1] = " + reqGrowthValues[i - 1]);
+            total += reqGrowthValues[i - 1];
+        }
+        total -= this.in_holds;
+        return total;
     }
 
     getTotalReqMeso() {
@@ -85,8 +86,30 @@ function doCalculate() {
     reqMesoID.innerHTML = "-";
     reqPeriodID.innerHTML = "-";
 
-    if((type === "0") || (level === 0) || isNaN(in_hold) || (in_hold < 0) || ((in_hold - cumulativeGrowthValue[level - 1] > 2679)) || (level === 20 && in_hold !== 0)) {
-        alert("값이 제대로 입력되지 않았습니다. (Value not selected correctly.)")
+    var ValidValue = isValidValue(type, level, in_hold);
+
+    if(ValidValue[6] !== 1) {
+
+        var alertString = "";
+        if(ValidValue[0] === 1) {
+            alertString += "심볼 종류가 선택되지 않았습니다\n(No symbol type selected)\n\n";
+        }
+        if (ValidValue[1] === 1) {
+            alertString += "심볼 레벨이 선택되지 않았습니다\n(No symbol type selected)\n\n";
+        }
+        if (ValidValue[2] === 1) {
+            alertString += "입력한 개수가 올바르지 않습니다\n(The number entered is not valid)\n\n";
+        }
+        if (ValidValue[3] === 1) {
+            alertString += "보유 중인 개수는 음수가 될 수 없습니다.\n(The number you hold cannot be negative)\n\n";
+        }
+        if (ValidValue[4] === 1) {
+            alertString += "최대 보유량을 초과했습니다.\n(Maximum retention exceeded.)\n\n";
+        }
+        if (ValidValue[5] === 1) {
+            alertString += "심볼이 완성됐고, 더 이상 보유할 수 없습니다\n(The symbol is complete, and can no longer be retained.)";
+        }
+        alert(alertString);
     } else {
         symbol = new Symbol(type, level, in_hold);
         console.log("reqSymbol : " + symbol.getTotalReqSymbol());
@@ -102,4 +125,49 @@ function doCalculate() {
         reqMesoID.innerHTML = formattedReqMeso + "메소";
         reqPeriodID.innerHTML = reqPeriod + "주" + "(" + (reqPeriod * 7) + "일)";
     }
+
+}
+
+
+function isValidValue(type, level, in_hold) {
+
+    var isValidValue = [0, 0, 0, 0, 0, 0, 0];
+
+    var totalInHold = 0;
+    for(var i = 0; i < level - 1; i++) {
+        totalInHold += reqGrowthValues[i];
+    }
+
+    if(type === "0") {
+        isValidValue[0] += 1;
+    }
+    if(level === 0) {
+        isValidValue[1] += 1;
+    }
+    if(isNaN(in_hold)) {
+        isValidValue[2] += 1;
+    } 
+    if(in_hold < 0) {
+        isValidValue[3] += 1;
+    }
+    if(in_hold + totalInHold > 2679) {
+        isValidValue[4] += 1;
+    }
+    if((level === 20 && in_hold !== 0)) {
+        isValidValue[5] += 1;
+    }
+    if(isAllZero(isValidValue)) {
+        isValidValue[6] += 1;
+    }
+
+    return isValidValue;
+}
+
+function isAllZero(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
